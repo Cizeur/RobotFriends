4e472f6d678154c1b8a4004f4f914dd5616c79a4
Cizeur <41877437+Cizeur@users.noreply.github.com>
2020-05-13 18:15:34 +0200

    added tachyons implementation to hello

--- src/Hello.js
+++ src/Hello.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import "./Hello.css";
+import "./Hello.css"
 class Hello extends Component {
 	render(){
 		return (
-			<div>
-			<h1>Hello World</h1>
-			<p>Welcome to React</p>
+			<div className="f1 tc">
+				<h1>Hello World</h1>
+				<p>Welcome to React</p>
 			</div>
 		)
 	}
