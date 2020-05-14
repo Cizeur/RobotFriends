25b97d72cd301c652d34674ec6a2d5fb3c6be77b
Cizeur <41877437+Cizeur@users.noreply.github.com>
2020-05-14 17:40:31 +0200

    added a loading screen

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ class App extends Component{
         || robots.email.toLowerCase().includes(this.state.searchfield.toLowerCase())
         )
     });
+    if (this.state.robots.length === 0) return <h1 className="tc f1">Loading</h1>
     return (
       <div className='tc'>
         <h1 className="f1">RoboFriends</h1>
