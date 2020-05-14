54d32dba51aa0439715b18aac4484ca35dae8392
Cizeur <41877437+Cizeur@users.noreply.github.com>
2020-05-14 16:36:34 +0200

    App back to a class to include STATE

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {robots} from './robots.js';
 import CardList from './CardList.js';
 import SearchBox from './SearchBox.js';
 
-const App = () =>{
-  return (
-    <div className='tc'>
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={robots}/>
-    </div>
-  )
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: '',
+    }
+  }
+  render(){
+    return (
+      <div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox />
+        <CardList robots={robots}/>
+      </div>
+    )
+  }
 }
 
 export default App;
