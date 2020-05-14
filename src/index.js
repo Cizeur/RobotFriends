a5eafba57c2badcccc3c003811754199f9e646af
Cizeur <41877437+Cizeur@users.noreply.github.com>
2020-05-14 11:09:45 +0200

    Reinstated app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-import Hello from './Hello.js';
+import App from './App';
+//import Hello from './Hello.js';
 import * as serviceWorker from './serviceWorker';
 import "tachyons"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Hello greeting={'Hello React Ninja'}/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
