0e9e776c43c979e8c48f5a59aa86caf929c746bf
Cizeur <41877437+Cizeur@users.noreply.github.com>
2020-05-14 17:52:20 +0200

    made the robot scrollable

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import CardList from './CardList.js';
 import SearchBox from './SearchBox.js';
+import Scroll from './Scroll.js';
+
 class App extends Component{
   // constructor
   constructor(){
@@ -35,7 +37,9 @@ class App extends Component{
       <div className='tc'>
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={filteredRobots}/>
+        <Scroll>
+          <CardList robots={filteredRobots}/>
+        </Scroll>
       </div>
     )
   }
