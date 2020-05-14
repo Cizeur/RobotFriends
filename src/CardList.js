f26a300f42a47de647de6c13d279a14297ece328
Cizeur <41877437+Cizeur@users.noreply.github.com>
2020-05-14 11:56:34 +0200

    complete robot list

--- src/CardList.js
+++ src/CardList.js
@@ -2,10 +2,15 @@ import React from 'react';
 import Card from './Card.js'
 
 const CardList = ({robots}) => {
+	const cardComponent = robots.map((user, index) => {
+		return <Card
+					id={robots[index].id}
+					name={robots[index].name}
+					email={robots[index].email} />
+	})
 	return (
 		<div>
-			<Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-			<Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+			{cardComponent}
 		</div>
 	);
 }
