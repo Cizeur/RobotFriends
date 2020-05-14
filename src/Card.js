de75545dcb54432ff8cd6a3ddb973ba05e0f2fad
Cizeur <41877437+Cizeur@users.noreply.github.com>
2020-05-14 11:20:09 +0200

    added robot image to a card

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = () => {
 	return (
 		<div>
-			<img alt='robot card' src=""></img>
+			<img alt='robot card' src="https://robohash.org/hellomyfriends?200x200"></img>
 			<div>
 				<h2>Jane Doe</h2>
 				<p>jane.doe@gmail.com</p>
