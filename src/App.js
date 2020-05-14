5807e02ef95ab7c6665a981b423947323417eac6
Cizeur <41877437+Cizeur@users.noreply.github.com>
2020-05-14 16:18:03 +0200

    Added a search box

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 import {robots} from './robots.js';
 import CardList from './CardList.js';
+import SearchBox from './SearchBox.js';
 
 const App = () =>{
   return (
     <Fragment>
     <h1>Robofriends</h1>
+    <SearchBox />
     <CardList robots={robots}/>
     </Fragment>
   )
---
+++ src/SearchBox.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchBox = () => {
+	return (
+		<input type='search' placeholder='Search Robot' />
+	)
+}
+
+export default SearchBox
