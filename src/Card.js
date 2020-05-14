2563abbac0aedb0647b595ca518fcddfc0f555e1
Cizeur <41877437+Cizeur@users.noreply.github.com>
2020-05-14 11:42:21 +0200

    displayed our first stored robot

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({name, email, id }) => {
 	return (
 		//light green background | inline-block | border 3 | padding 3 | margin 3 | grow on hover | border width 2 and shadow 5
-		<div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img alt='robot card' src="https://robohash.org/hellomyfriends?200x200"></img>
+		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+			<img alt='robot card' src={`https://robohash.org/${id}?200x200`}></img>
 			<div>
-				<h2>Jane Doe</h2>
-				<p>jane.doe@gmail.com</p>
+				<h2>{name}</h2>
+				<p>{email}</p>
 			</div>
 		</div>
 	);
