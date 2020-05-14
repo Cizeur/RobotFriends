57f073f16d784ab42cafb5bff6730acc92495eed
Cizeur <41877437+Cizeur@users.noreply.github.com>
2020-05-14 11:33:22 +0200

    further tachyon styling

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Card = () => {
 	return (
-		//light green background | inline-block | border 3 | padding 3 | margin 3 | grow on hover
-		<div className='bg-light-green dib br3 pa3 ma2 grow'>
+		//light green background | inline-block | border 3 | padding 3 | margin 3 | grow on hover | border width 2 and shadow 5
+		<div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
 			<img alt='robot card' src="https://robohash.org/hellomyfriends?200x200"></img>
 			<div>
 				<h2>Jane Doe</h2>
