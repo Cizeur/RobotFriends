9a92c9f04aa621a8d721e2ea6c2c6e9a71bb62b0
Cizeur <41877437+Cizeur@users.noreply.github.com>
2020-05-15 10:55:43 +0200

    mistaken in size attribute for robot pictures

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = ({name, email, id }) => {
 	return (
 		//light green background | inline-block | border 3 | padding 3 | margin 3 | grow on hover | border width 2 and shadow 5
 		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img alt='robot card' src={`https://robohash.org/${id}?200x200`}></img>
+			<img alt='robot card' src={`https://robohash.org/${id}?size=200x200`}></img>
 			<div>
 				<h2>{name}</h2>
 				<p>{email}</p>
