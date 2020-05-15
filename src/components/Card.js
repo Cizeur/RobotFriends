4db13e616c20a496b25e75939217ab9795ef087a
Cizeur <41877437+Cizeur@users.noreply.github.com>
2020-05-15 17:33:28 +0200

    prettified

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React from "react";
 
-const Card = ({name, email, id }) => {
-	return (
-		//light green background | inline-block | border 3 | padding 3 | margin 3 | grow on hover | border width 2 and shadow 5
-		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img alt='robot card' src={`https://robohash.org/${id}?size=200x200`}></img>
-			<div>
-				<h2>{name}</h2>
-				<p>{email}</p>
-			</div>
-		</div>
-	);
-}
+const Card = ({ name, email, id }) => {
+  return (
+    //light green background | inline-block | border 3 | padding 3 | margin 3 | grow on hover | border width 2 and shadow 5
+    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img
+        alt="robot card"
+        src={`https://robohash.org/${id}?size=200x200`}
+      ></img>
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { createLogger } from 'redux-logger';
-import thunkMiddleware from 'redux-thunk';
-import './index.css';
-import App from './containers/App.js';
-import * as serviceWorker from './serviceWorker';
-import { searchRobots } from './reducers';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
+import "./index.css";
+import App from "./containers/App.js";
+import * as serviceWorker from "./serviceWorker";
+import { searchRobots } from "./reducers";
 import "tachyons";
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(thunkMiddleware, logger));
+const store = createStore(
+  searchRobots,
+  applyMiddleware(thunkMiddleware, logger)
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store} >
+    <Provider store={store}>
       <App />
     </Provider>
-
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
