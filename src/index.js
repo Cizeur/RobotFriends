547ce5cca66d36a994a7ac8240e1c4f5aa85ee19
Cizeur <41877437+Cizeur@users.noreply.github.com>
2020-05-15 17:56:22 +0200

    prettified

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware , combineReducers} from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import { createLogger } from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 import "./index.css";
@@ -11,7 +11,7 @@ import { searchRobots, requestRobots } from "./reducers";
 import "tachyons";
 
 const logger = createLogger();
-const rootReducer = combineReducers({searchRobots, requestRobots})
+const rootReducer = combineReducers({ searchRobots, requestRobots });
 const store = createStore(
   rootReducer,
   applyMiddleware(thunkMiddleware, logger)
